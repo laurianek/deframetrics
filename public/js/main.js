f307f2a475191ef0c0bafa9b9af3490baf7812bd
Guy G <guy.griffiths@rdg.ac.uk>
2017-01-04 12:07:17 +0000

    Removed jquery and changed main.js to reflect that

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
-import $ from 'jquery'
+/* Main code entrypoint.  This file should generally just call other JS modules and hook the elements up */
+console.log('Document ready')
 
-$(document).ready(() => {
-    /* Main code entrypoint.  This file should generally just call other JS modules and hook the elements up */
-    console.log('Document NOT! ready')
-});
+/* Note that this works because this script doesn't get loaded until the end of <body>.
+   If you're writing something to be used by other people, which could go *anywhere* and does something immediately, (rather than just supplying a class etc.) then you'll need jQuery's ready() or an equivalent. */
